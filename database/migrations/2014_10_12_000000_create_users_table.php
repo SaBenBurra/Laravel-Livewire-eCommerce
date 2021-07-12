9a3784267a251a0cf34965b1923c27659dbd8a54
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-12 17:40:48 +0300

    add is_banned and is_admin columns to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_banned')->default(0);
+            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
