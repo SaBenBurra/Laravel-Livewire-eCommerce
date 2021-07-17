8f361af2e22f6253b75b15e3fd0cf21fbf0a9d31
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-17 22:06:09 +0300

    create ProductProperty model and resource controller

---
+++ app/Http/Controllers/Admin/ProductPropertyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductProperty;
+use Illuminate\Http\Request;
+
+class ProductPropertyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductProperty  $productProperty
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductProperty $productProperty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductProperty  $productProperty
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductProperty $productProperty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductProperty  $productProperty
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductProperty $productProperty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductProperty  $productProperty
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductProperty $productProperty)
+    {
+        //
+    }
+}
