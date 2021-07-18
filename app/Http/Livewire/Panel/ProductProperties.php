16a6f1d338d1f91828b9fc5d27a2aba39956c51e
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-19 00:24:49 +0300

    create product properties livewire component

---
+++ app/Http/Livewire/Panel/ProductProperties.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Panel;
+
+use Livewire\Component;
+
+class ProductProperties extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.product-properties');
+    }
+}
