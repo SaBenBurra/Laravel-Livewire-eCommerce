1b28d543c5e8fbb81871964cef56503d525c340e
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-19 10:28:56 +0300

    define relationship beetween product properties and product property values and names

--- app/Models/ProductProperty.php
+++ app/Models/ProductProperty.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class ProductProperty extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function propertyName()
+    {
+        return $this->hasOne(ProductPropertyName::class, 'id', 'property_name_id');
+    }
+
+    public function propertyValue()
+    {
+        return $this->hasOne(ProductPropertyValue::class, 'id', 'property_value_id');
+    }
 }
