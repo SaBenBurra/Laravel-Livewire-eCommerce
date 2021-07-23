c78b6760f4bd2061b594c8d607cf65380da55b8a
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-23 20:07:56 +0300

    create category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['clothes', 'phone', 'furniture', 'book', 'jewelry'];
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProductPropertySeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
