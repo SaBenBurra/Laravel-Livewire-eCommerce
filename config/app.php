875f8bba77a84eec7299cf51803e58b1f57290fe
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-26 20:23:29 +0300

    install fortify

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        \App\Providers\FortifyServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
