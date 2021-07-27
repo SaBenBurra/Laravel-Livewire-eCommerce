6fae2a7f3ed8f1bad94be30f33040a7e29b41316
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-27 19:38:36 +0300

    create UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             BrandSeeder::class,
             ProductSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
