5b60631928e1f6c5c61e97164d7e7180becd3eac
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-07-30 17:07:28 +0300

    solve naming mistake

--- database/migrations/2021_05_30_170326_create_cart_items_table.php
+++ database/migrations/2021_05_30_170326_create_cart_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChartItemsTable extends Migration
+class CreateCartItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateChartItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('chart_items', function (Blueprint $table) {
+        Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('user_id');
@@ -40,6 +40,6 @@ class CreateChartItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('chart_items');
+        Schema::dropIfExists('cart_items');
     }
 }
