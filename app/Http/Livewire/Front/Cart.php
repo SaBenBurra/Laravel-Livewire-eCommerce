66637d86788839733af4f75e746773f1fbaeecfd
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-08-01 00:57:36 +0300

    create cart livewire component and place at the cart page

---
+++ app/Http/Livewire/Front/Cart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Front;
+
+use Livewire\Component;
+
+class Cart extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.cart');
+    }
+}
