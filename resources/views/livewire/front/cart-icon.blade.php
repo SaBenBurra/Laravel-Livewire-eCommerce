32895a708f6bb83a91809757854f79f49f186577
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-08-01 00:55:04 +0300

    link the cart icon to cart page

--- resources/views/livewire/front/cart-icon.blade.php
+++ resources/views/livewire/front/cart-icon.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+    <a href="{{route('front.cart')}}" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
     <span class="badge badge-pill badge-danger notify">{{$chartCount}}</span>
 </div>
