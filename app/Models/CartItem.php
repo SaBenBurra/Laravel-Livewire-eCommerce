12a291e89850796992abd7520aecb33598b7a9da
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-08-02 01:07:27 +0300

    define relations for cart item model

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -10,4 +10,28 @@ class CartItem extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function variants()
+    {
+        $variants = [];
+        foreach (json_decode($this->variants) as $variantId) {
+            array_push($variants, ProductVariant::find($variantId));
+        }
+        return $variants;
+    }
+
+    public function price()
+    {
+        foreach ($this->variants() as $variant) {
+            if ($variant->is_price_using === 1) {
+                return $variant->price;
+            }
+        }
+        return $this->product->price;
+    }
 }
