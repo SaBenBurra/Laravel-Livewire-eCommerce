4f4747779036818b02a93b2c0c5623a36c779378
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-08-19 17:21:54 +0300

    create address model resource controller

---
+++ app/Http/Controllers/Front/AddressController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Address;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Address $address)
+    {
+        //
+    }
+
+    public function edit(Address $address)
+    {
+        //
+    }
+
+    public function update(Request $request, Address $address)
+    {
+        //
+    }
+
+    public function destroy(Address $address)
+    {
+        //
+    }
+}
